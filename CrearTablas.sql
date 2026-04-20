879e9ddad69fc8b61f727e5f1b19d7119a525bf3
CarlosAndres1204 <81260603+CarlosAndres1204@users.noreply.github.com>
2026-04-20 15:29:30 -0600

    Create CrearTablas.sql
    Estos son las tabalas, basicas con algunas observaciones como comentarios, los constraint los voy a montar en un archivo aparte.

---
+++ CrearTablas.sql
@@ -0,0 +1,318 @@
+CREATE TABLE PetLevelEnergy (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetBreed (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetType (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetState (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetLillness (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    Description VARCHAR(100)
+);
+
+CREATE TABLE Medicine (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    Dose VARCHAR2(25)
+);
+
+CREATE TABLE Treatment (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    Description VARCHAR(100)
+);
+
+CREATE TABLE PetTraining (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetPhoto (
+    Id NUMBER(8),
+    Photo LONG,
+    IdPet Number(8)
+);
+
+CREATE TABLE SpaceRequired (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetSize (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE District (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    IdCanton Number(8)
+);
+
+CREATE TABLE Canton (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    IdProvince Number(8)
+);
+
+CREATE TABLE Province (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    IdCountry Number(8)
+);
+
+CREATE TABLE Country (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE PetSeverity (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE Pet (
+    Id NUMBER(8),
+    Color VARCHAR2(50),
+    Age Number(8),
+    Description VARCHAR2(100),
+    Name VARCHAR2(25),
+    Chip VARCHAR2(15),
+    IdEnergy NUMBER(8),
+    IdState NUMBER(8),
+    IdType NUMBER(8),
+    IdBreed NUMBER(8),
+    IdDistrict NUMBER(8),
+    IdSpace NUMBER(8),
+    IdTraining NUMBER(8),
+    IdSize NUMBER(8),
+    IdOwner NUMBER(8),
+    IdVeterinarian NUMBER(8)
+);
+
+CREATE TABLE Veterinarian (
+    Id NUMBER(8),
+    Email VARCHAR2(25),
+    FirstName VARCHAR2(25),
+    LastName VARCHAR2(25),
+    Location VARCHAR2(100), -- Agregar Distrito
+    Phone NUMBER(8),
+    Name VARCHAR2(50)
+);
+
+CREATE TABLE SizeXFosterHome (
+    IdSize NUMBER(8),
+    IdFosterHome NUMBER(8)
+);
+
+CREATE TABLE PetXTreatment (
+    IdPet NUMBER(8),
+    IdTreatment NUMBER(8)
+);
+
+CREATE TABLE PetXPetIllness (
+    IdPet NUMBER(8),
+    IdPetIllness NUMBER(8)
+);
+
+CREATE TABLE PetXMedicine (
+    IdPet NUMBER(8),
+    IdMedicine NUMBER(8)
+);
+
+CREATE TABLE PetLevelEnergyXFosterHome (
+    IdEnergy NUMBER(8),
+    IdFosterHome NUMBER(8)
+);
+
+CREATE TABLE SpaceRequiredXFosterHome (
+    IdSpaceRequired NUMBER(8),
+    IdFosterHome NUMBER(8)
+);
+
+CREATE TABLE FoundReport (
+    Id NUMBER(8),
+    Date DATE,
+    Place VARCHAR2(100), -- podria ligarse a district
+    Description VARCHAR2(100),
+    IdPet NUMBER(8),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Match (
+    Id NUMBER(8),
+    SimilarityPercentage NUMBER(3),
+    MatchDate DATE,
+    Status VARCHAR2(25), -- Esto debe ser un campo definido y no me acuerdo porque esto tiene estado
+    IdLostRepost NUMBER(8),
+    IdFoundReport NUMBER(8)
+);
+
+CREATE TABLE LostReport (
+    Id NUMBER(8),
+    Date DATE,
+    Place VARCHAR2(100), -- podria ligarse a district
+    Description VARCHAR2(100),
+    Reward NUMBER(8),
+    State VARCHAR2(25), -- Esto debe ser un campo definido
+    IdPet NUMBER(8),
+    IdCurrency NUMBER(8)
+);
+
+CREATE TABLE Adoption (
+    Id NUMBER(8),
+    AdoptionDate DATE,
+    AvailableDate DATE,
+    Description VARCHAR2(100),
+    Amount NUMBER(8), -- no me acuerdo para que es esto
+    State VARCHAR2(25), -- Esto debe ser un campo definido
+    IdPet NUMBER(8),
+    IdAdopter NUMBER(8),
+    IdOwner NUMBER(8)
+);
+
+CREATE TABLE Rescued (
+    Id NUMBER(8),
+    Date DATE,
+    Place VARCHAR2(100), -- esto se podria ligar a distric
+    BeforePhoto LONG,
+    AfterPhoto LONG,
+    Description VARCHAR2(100),
+    IdPet NUMBER(8),
+    IdRescuer NUMBER(8),
+    IdSeverity NUMBER(8)
+);
+
+CREATE TABLE Donation (
+    Id NUMBER(8),
+    Amount NUMBER(8),
+    Date Date,
+    IdUser NUMBER(8),
+    IdCurrency NUMBER(8),
+    IdAssociation NUMBER(8)
+);
+
+CREATE TABLE Currency (
+    Id NUMBER(8),
+    Name VARCHAR2(25)
+);
+
+CREATE TABLE User (
+    Id NUMBER(8),
+    FirstName VARCHAR2(25),
+    LastName VARCHAR2(25),
+    Password VARCHAR2(15),
+    Notes VARCHAR2(25), -- No se porque tiene Notas el usuario, quiza deberiamos meterles Distric
+    IdEmail NUMBER(8),
+    IdFosterHome NUMBER(8) -- el modelo logico esta mal con lo que viene despues :p
+);
+
+CREATE TABLE ReportList (
+    Id NUMBER(8),
+    Description VARCHAR2(250),
+    IdUser NUMBER(8),
+    IdReporter NUMBER(8), -- estaria bien agregar a quien hizo el reporte
+    DateReport DATE -- estaria bien agregar la fecha del reporte
+);
+
+CREATE TABLE Rescuer (
+    Id NUMBER(8),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Adopter (
+    Id NUMBER(8),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Calification (
+    Id NUMBER(8),
+    Stars NUMBER(8),
+    Note VARCHAR2(250),
+    Date DATE,
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE FosterHome (
+    Id NUMBER(8),
+    NeedsDonation VARCHAR2(1),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Admin (
+    Id NUMBER(8),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Email (
+    Id NUMBER(8),
+    Email VARCHAR2(25),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Phone (
+    Id NUMBER(8),
+    Phone NUMBER(8),
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE BlockList (
+    Id NUMBER(8),
+    Date DATE,
+    IdUser NUMBER(8)
+);
+
+CREATE TABLE Association (
+    Id NUMBER(8),
+    Name VARCHAR2(25),
+    PhoneNumber NUMBER(8),
+    BankAccount VARCHAR2(35),
+    Email VARCHAR2(25)
+);
+
+CREATE TABLE Deleted (
+    Id NUMBER(8),
+    DeletedDate DATE,
+    DeletedBy VARCHAR2(25), --esto podria ser el id del usuario
+    TableName VARCHAR2(25),
+    DeletedId NUMBER(8)
+);
+
+CREATE TABLE Created (
+    Id NUMBER(8),
+    CreatedDate DATE,
+    CreatedBy VARCHAR2(25), --esto podria ser el id del usuario
+    TableName VARCHAR2(25),
+    CreatedId NUMBER(8)
+);
+
+CREATE TABLE Parameter (
+    Id NUMBER(8),
+    Value NUMBER(8),
+    Name VARCHAR2(25),
+    Description VARCHAR2(100)
+);
+
+CREATE TABLE Bitacora ( -- Ojo
+    Id NUMBER(8),
+    TableName VARCHAR2(25),
+    ChangeDate DATE, 
+    PreviousValue VARCHAR2(50),
+    ChangedBy NUMBER(8),
+    CurrentValue VARCHAR2(50),
+    FieldName VARCHAR2(25)
+);
