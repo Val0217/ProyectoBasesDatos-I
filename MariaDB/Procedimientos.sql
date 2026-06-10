f227d2874d25e9f5dd5e3a919a9238f8fe3ca0e8
CarlosAndres1204 <81260603+CarlosAndres1204@users.noreply.github.com>
2026-06-09 18:56:24 -0600

    Update Procedimientos.sql

--- MariaDB/Procedimientos.sql
+++ MariaDB/Procedimientos.sql
@@ -1593,13 +1593,8 @@ BEGIN
         SET v_available_date = NOW();
     END IF;
 
-    /* Generar ID (manteniendo lógica actual) */
-    SELECT fn_next_id('Adoption')
-    INTO p_new_id;
-
     /* Insert solicitud */
     INSERT INTO Adoption (
-        Id,
         AdoptionDate,
         AvailableDate,
         Description,
@@ -1609,7 +1604,6 @@ BEGIN
         IdOwner
     )
     VALUES (
-        p_new_id,
         NULL,
         v_available_date,
         SUBSTRING(p_description, 1, 100),
@@ -1620,6 +1614,7 @@ BEGIN
     );
 
     COMMIT;
+    SET p_new_id = LAST_INSERT_ID();
 
 END $$
 
